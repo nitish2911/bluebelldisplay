eec9961507b9e95f860055facd2a9f96ceed5b57
nitish2911 <109421451+nitish2911@users.noreply.github.com>
2026-01-16 20:25:15 +0530

    custom domain setting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bluebelldisplay/'   // 👈 EXACT GitHub repo name
+  base: '/'   //Custom domain
 })
